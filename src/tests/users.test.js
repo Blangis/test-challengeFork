45893aa1964cbf90b1f24e72cc7deeb3adccd1e6
Blangis <blancag.licona@gmail.com>
2024-05-07 00:52:20 -0600

    prueba ejercicio :(

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -1,9 +1,51 @@
 // You already have the methods imported
+/*
 import { default as expect } from "expect";
-import { getUser, getUsers } from "../users";
+import { getUser, getUsers, getUsuario } from "../users";
+*/
+
+const users = require("../data.js");
+
+const expect = require("expect").default;
+const { 
+    getUser,
+    getUsers,
+    getUsuario,
+} = require("../users.js");
+
 
 // Make a test to verify if the method getUser(id) works correctly
 // Verify any data of the resulting object you want
 
 // READ documentation
 
+describe("getUser", () => {
+    it("It should retrieve the correct user for a given ID", async () => {
+        const user = users[0];
+        expect(getUsuario(1)).toEqual(user); 
+    });
+    it("It should retrieve the correct data for a given ID", async () =>{
+        const userId = 1;
+        const expectedUser ={
+            id: 1,
+            username: "hpLover4",
+            email: "sirious90@gmail.com",
+            password: "hArrydotCom",
+
+        };
+        const user = await getUser(userId);
+        expect(user).toEqual(expectedUser);
+
+    });
+    it("should return an error for an invalid ID", async () => {
+        const fakeID= 8;
+
+        try {
+            await getUser(fakeID);
+            fail("getUser should throw a mistake for invalid ID");
+        } catch (error) {
+            expect(error.message).toBe("User not found");
+        }
+    });
+
+});
--- src/users.js
+++ src/users.js
@@ -8,12 +8,47 @@ const getUsers = () => {
 };
 
 // Filters users by specific ID
-const getUser = (id) => {
+const getUsuario = (id) => {
   return users.find((user) => user.id === id);
 };
+
+//Fetch
+/*
+const getUser (id) {
+  try {
+    const respond= await fetch(users+id)
+
+    const data = await respond.json();
+    console.log(data);
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+*/
+
+
+// Promise 
+
+const getUser = (id) => {
+  return new Promise((resolve, reject) => {
+    console.log(`Verificando id: ${id} de usuario`);
+    setTimeout(() =>{
+      const user= users.find((user) => user.id === id);;
+      console.log(`Verificación id: ${id} de usuario terminada`);
+      if(user){
+      resolve(user);
+    } else {
+      reject(new Error("Usuario no encontrado"));
+    }
+  }, 2000);
+});
+};
+
+
 
 // test
 // console.log(getUser(3));
 
-module.exports = { getUsers, getUser };
+module.exports = { getUsers, getUser, getUsuario };
 
